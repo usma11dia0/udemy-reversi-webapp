050a8f44046a011b8b95a1f1dddebbad7e8c7770
usma11dia0 <usma11dia0@gmail.com>
2023-09-26 23:34:58 +0900

    add: nodemon and morgan

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import morgan from 'morgan'
+import 'express-async-errors'
+
+const PORT = 3000
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get('/api/hello', async (req, res) => {
+  res.json({
+    message: 'Hello Express!!'
+  })
+})
+
+app.get('/api/error', async (req, res) => {
+  throw new Error('Error endpoint')
+})
+
+app.use(errorHander)
+
+app.listen(PORT, () => {
+  console.log(`Reversi application started: http://localhost:${PORT}`)
+})
+
+function errorHander(
+    err: any, 
+    _req: express.Request, 
+    res: express.Response, 
+    _next: express.NextFunction
+) {
+  console.error('Unexpected error occurred', err)
+  res.status(500).send({
+    message: 'Unexpected error occured'
+  })
+}
